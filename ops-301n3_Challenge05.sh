987ab8c24d6d522a50a9e32499ab997978a17ee8
OddGarden <oddgarden@outlook.com>
2023-05-10 23:51:43 +0000

    added new challenge

---
+++ ops-301n3_Challenge05.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+# Script: 		    OPS 301 Challenge 05
+# Author: 		    Lilian Mburu
+# Last Revision: 	May 6rd 2023
+# Purpose: 		    A script that clears log files for you.              
+#     
+# Variables
+# Functions
+# Main
+# End
